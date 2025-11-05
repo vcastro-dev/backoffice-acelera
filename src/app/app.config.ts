b7f36157dacca086ffb20567d758f71d3fea3732
@vcastro-dev <vcastro2395@gmail.com>
2025-11-05 01:32:26 -0300

    style: cria tema customizado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
+import { CustomPreset } from './themes/presets/custom-preset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    providePrimeNG(),
+    providePrimeNG({
+      theme: {
+        preset: CustomPreset,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+    }),
   ],
 };
---
+++ src/app/themes/presets/custom-preset.ts
@@ -0,0 +1,46 @@
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
+
+const BRAND_BLUE = '#0D99FF';
+const STATUS_GREEN = '#28a745';
+const STATUS_INFO_BLUE = '#3B82F6';
+const STATUS_RED = '#DC3545';
+const WHITE = '#FFFFFF';
+
+export const CustomPreset = definePreset(Aura, {
+  name: 'custom-preset',
+  semantic: {
+    colorScheme: {
+      light: {
+        primary: {
+          color: BRAND_BLUE,
+          contrast: WHITE,
+
+          hover: {
+            color: '#0B84DB',
+            contrast: WHITE,
+          },
+        },
+
+        success: {
+          color: STATUS_GREEN,
+          contrast: WHITE,
+        },
+        info: {
+          color: STATUS_INFO_BLUE,
+          contrast: WHITE,
+        },
+        danger: {
+          color: STATUS_RED,
+          contrast: WHITE,
+        },
+
+        surface: {
+          ground: '{color.gray.50}',
+          default: '{color.white}',
+        },
+      },
+      dark: {},
+    },
+  },
+});
