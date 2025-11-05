50ca370a0bfc7d82889640113ee8cac8375c2981
@vcastro-dev <vcastro2395@gmail.com>
2025-11-05 16:02:22 -0300

    fix: corrige display do calendário

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { CustomPreset } from './themes/presets/custom-preset';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: CustomPreset,
