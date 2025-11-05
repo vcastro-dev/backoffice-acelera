bf497fc337c7826d0e46818fce95ba8f00effd9b
@vcastro-dev <vcastro2395@gmail.com>
2025-11-05 15:15:27 -0300

    feat: adiciona componentes datepicker

--- src/app/components/search-box/search-box.ts
+++ src/app/components/search-box/search-box.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { DataViewModule } from 'primeng/dataview';
+import { DatePickerModule } from 'primeng/datepicker';
 import { DividerModule } from 'primeng/divider';
+import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-search-box',
-  imports: [ButtonModule, CardModule, DividerModule, InputTextModule],
+  imports: [
+    ButtonModule,
+    CardModule,
+    DataViewModule,
+    DatePickerModule,
+    DividerModule,
+    FloatLabelModule,
+    InputTextModule,
+  ],
   templateUrl: './search-box.html',
   styleUrl: './search-box.css',
   standalone: true,
