dee3e9574df746b93f0a392724f09e39c446595c
@vcastro-dev <vcastro2395@gmail.com>
2025-12-04 00:06:29 -0300

    feat: cria wrapper para alinhar o conteúdo do layout

--- src/app/shared/components/app-layout/app-layout.ts
+++ src/app/shared/components/app-layout/app-layout.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 @Component({
   selector: 'app-app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menubar],
   templateUrl: './app-layout.html',
   styleUrl: './app-layout.css',
 })
-export class AppLayout {}
+export class AppLayout {
+  options: MenuItem[] = [
+    {
+      label: 'Home',
+      routerLink: '/',
+    },
+    {
+      label: 'Schemas',
+      routerLink: '/schemas',
+    },
+  ];
+}
