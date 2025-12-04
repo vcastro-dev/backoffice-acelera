b9685c66343b1d1659a7677967b1fb011d074be1
@vcastro-dev <vcastro2395@gmail.com>
2025-12-03 22:50:23 -0300

    feat: cria componente de layout da aplicação

---
+++ src/app/shared/components/app-layout/app-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-app-layout',
+  imports: [RouterOutlet],
+  templateUrl: './app-layout.html',
+  styleUrl: './app-layout.css',
+})
+export class AppLayout {}
