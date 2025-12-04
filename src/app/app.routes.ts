6dc63f6e38530a98cd84d7a7e9df5863d1778acf
@vcastro-dev <vcastro2395@gmail.com>
2025-12-03 22:50:30 -0300

    feat: configura rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './shared/components/app-layout/app-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: AppLayout,
+  },
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      {
+        path: 'integration-logs',
+        loadChildren: () =>
+          import('./features/integrations-logs/integration-logs.routes').then(
+            (m) => m.INTEGRATIONS_LOGS_ROUTES
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'integration-logs',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'integration-logs',
+  },
+];
---
+++ src/app/features/integrations-logs/integration-logs.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { IntegrationsLogs } from './integrations-logs';
+
+export const INTEGRATIONS_LOGS_ROUTES: Routes = [{ path: '', component: IntegrationsLogs }];
